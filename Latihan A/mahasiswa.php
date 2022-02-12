d317e21d95766c0c61972d30cb4f5bd7c8b0d53d
Shafa Meira Wahyono <71260611+meirashaf@users.noreply.github.com>
2022-02-12 23:06:00 +0700

    Add files via upload

---
+++ Latihan A/mahasiswa.php
@@ -0,0 +1,61 @@
+<?php
+    class mahasiswa{
+        private $Nama = "Nama";
+        private $NIM = 0;
+        private $jenisKelamin = "jenisKelamin";
+        private $prodi = "prodi";
+        private $Semester = 0;    
+
+        public function __construct($Nama="Nama", $NIM=0, $jenisKelamin="jenisKelamin", $prodi="prodi", $Semester=0){
+            $this->Nama = $Nama;
+            $this->NIM = $NIM;
+            $this->jenisKelamin = $jenisKelamin;
+            $this->prodi = $prodi;
+            $this->Semester = $Semester;
+        }
+
+        public function setNama($Nama){
+            $this->Nama = $Nama;
+        }
+        public function getNama(){
+            return $this->Nama;
+        }
+
+        public function setNIM($NIM){
+            $this->NIM = $NIM;
+        }
+        public function getNIM(){
+            return $this->NIM;
+        }
+
+        public function setJenisKelamin($jenisKelamin){
+            $this->jenisKelamin = $jenisKelamin;
+        }
+        public function getJenisKelamin(){
+            return $this->jenisKelamin;
+        }
+
+        public function setProdi($prodi){
+            $this->prodi = $prodi;
+        }
+        public function getProdi(){
+            return $this->prodi;
+        }
+
+        public function setSemester($Semester){
+            $this->Semester = $Semester;
+        }
+        public function getSemester(){
+            return $this->Semester;
+        }
+
+        public function print(){
+            echo "==============================================<br>";
+            echo "Nama : ".$this->Nama."<br>";
+            echo "NIM : ".$this->NIM."<br>";
+            echo "Jenis Kelamin : ".$this->jenisKelamin."<br>";
+            echo "Program Studi : ".$this->prodi."<br>";
+            echo "Semester : ".$this->Semester."<br>";
+        }
+    }
+?>
