32092e9d673a4b82a947e8957a633580e0cece4e
Shafa Meira Wahyono <71260611+meirashaf@users.noreply.github.com>
2022-02-12 23:26:11 +0700

    Add files via upload

--- Latihan A/index.php
+++ Latihan A/index.php
@@ -1,11 +1,13 @@
 <?php
     include "mahasiswa.php";
 
+    // menginstansiasi objek
     $satu = new mahasiswa("Shafa Meira W.", 2007723, "Perempuan", "Ilmu Komputer", 4);
     $dua = new mahasiswa("Alfi Amaliandini", 2005647, "Perempuan", "Matematika", 5);
     $tiga = new mahasiswa("M. Rayzahran", 2001219, "Laki-laki", "Hubungan Internasional", 2);
     $empat = new mahasiswa("Dhiandra Latifa", 2009892, "Perempuan", "Desain dan Komunikasi Visual", 3);
 
+    // menampilkan output dengan memanggil fungsi print
     echo $satu->print();
     echo $dua->print();
     echo $tiga->print();
--- Latihan A/mahasiswa.php
+++ Latihan A/mahasiswa.php
@@ -1,11 +1,13 @@
 <?php
     class mahasiswa{
+        // atribut
         private $Nama = "Nama";
         private $NIM = 0;
         private $jenisKelamin = "jenisKelamin";
         private $prodi = "prodi";
         private $Semester = 0;    
 
+        // constructor
         public function __construct($Nama="Nama", $NIM=0, $jenisKelamin="jenisKelamin", $prodi="prodi", $Semester=0){
             $this->Nama = $Nama;
             $this->NIM = $NIM;
@@ -13,6 +15,10 @@
             $this->prodi = $prodi;
             $this->Semester = $Semester;
         }
+
+        /*
+            Setter dan Getter
+        */ 
 
         public function setNama($Nama){
             $this->Nama = $Nama;
@@ -49,6 +55,7 @@
             return $this->Semester;
         }
 
+        // untuk menampilkan semua atribut per objek
         public function print(){
             echo "==============================================<br>";
             echo "Nama : ".$this->Nama."<br>";
